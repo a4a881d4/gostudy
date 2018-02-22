312a9c9e922d183ce1db504ad1a1601f2a070c38
a4a881d4 <a4a881d4@163.com>
2018-02-22 21:41:11 +0800

    read private key

--- ethereum/account/readkeyfile.go
+++ ethereum/account/readkeyfile.go
@@ -4,24 +4,31 @@ import (
 	"github.com/ethereum/go-ethereum/accounts/keystore"
 	"fmt"
 	"os"
+	"math/big"
 )
 
+//go run ethereum/account/readkeyfile.go ../../ethereum/chain/keystore 3A703C956f29Da6666d681fd143170f9a84D20db 931
 func main() {
-	dir := os.argv[1]
-	address := os.argv[2]
-	passphase := os.argv[3]
+	dir := os.Args[1]
+	var address = new(big.Int)
+	address.SetString(os.Args[2],16)
+	passphase := os.Args[3]
 
-	fmt.Println("Form key store: ",dir,"read address: ",address,"private key")
+	fmt.Println("Form key store: ",dir,"read address: ",address.Text(16),"private key")
 
 	ks := keystore.NewKeyStore(dir, 262144, 1)
 	accounts := ks.Accounts()
-	if keyjson, err := ks.Export(accounts[0], "931", "931"); err != nil {
-		fmt.Println("Exort",err)
-	} else {
-		if key,err := keystore.DecryptKey(keyjson, "931"); err != nil {
-			fmt.Println("Exort",err)
-		} else {
-			fmt.Println(key.PrivateKey.D.Text(16))
+	for i,a := range accounts {
+		if address.Sub(a.Address.Big(), address).Sign() == 0 {
+			if keyjson, err := ks.Export(accounts[i], passphase, passphase); err != nil {
+				fmt.Println("Exort",err)
+			} else {
+				if key,err := keystore.DecryptKey(keyjson, passphase); err != nil {
+					fmt.Println("Exort",err)
+				} else {
+					fmt.Println(key.PrivateKey.D.Text(16))
+				}
+			}
 		}
 	}
 }
